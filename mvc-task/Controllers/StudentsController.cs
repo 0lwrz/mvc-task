c1aa1cd657c2141d947f7fb406a0f3f22acaeb91
Sami yassen <samiyaseen970@gmail.com>
2024-08-25 08:34:23 +0300

    mvc

---
+++ mvc-task/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_task.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult getAll()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
